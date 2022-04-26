3af8dd20f418d8628ebd094b151f6c6e09255dfe
Sabin Acharya <sabinacharya444@gmail.com>
2022-04-26 20:28:20 +1000

    read me updated

--- src/component/Header.js
+++ src/component/Header.js
@@ -5,8 +5,7 @@ export default function Header(){
         <div className="header">
              <a href="index.html" className="header--title">               
                 <i class="fas fa-earth-oceania"></i>                      
-                <h2 className="header--title">my travel journal.</h2>
-                  
+                <h2 className="header--title">my travel journal.</h2>   
              </a>
          </div>
     )
