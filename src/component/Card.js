971ec0ceeea475075952503e592dd33a7c3aa3f7
Sabin Acharya <sabinacharya444@gmail.com>
2022-04-26 15:01:26 +1000

    header and card added

--- src/component/Card.js
+++ src/component/Card.js
@@ -2,8 +2,22 @@ import React from "react"
 
 export default function Card(){
     return(
+        <div>
         <div className="card">
-            <h1>Card</h1>
+            <img className="card--image" src="https://source.unsplash.com/WLxQvbMyfas"></img>
+            <div className="card--content">
+                <div className="card--location">
+                    <i class="fa-solid fa-location-dot"></i>
+                    <p className="card--locationtitle">JAPAN<span className="card--googlemap"><a className="card--googlemaplink" href="www.google.com">View on Google Maps</a></span></p>
+                </div>
+                <h2 className="card--title">Mount Fiji</h2>
+                <p className="card--date strong">12 Jan, 2021 - 24 Jan, 2021</p>
+                <p className="card--description">
+                Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.
+                </p>
+            </div>
+        </div>
+        <div className="card--line"></div>
         </div>
     )
 }
--- src/component/Header.js
+++ src/component/Header.js
@@ -3,7 +3,11 @@ import React from "react"
 export default function Header(){
     return(
         <div className="header">
-            <h1>Header</h1>
-        </div>
+             <a href="index.html" className="header--title">               
+                <i class="fas fa-earth-oceania"></i>                      
+                <h2 className="header--title">my travel journal.</h2>
+                  
+             </a>
+         </div>
     )
 }
